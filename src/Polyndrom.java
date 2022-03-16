4100b5a277e3e44850bff2ed4008abe1a6f6fcb4
Sergei Egorov <esv89217763366@yandex.ru>
2022-03-16 13:01:32 +0300

    var2

---
+++ src/Polyndrom.java
@@ -0,0 +1,16 @@
+//Определить, является ли срока полиндромом.
+// не работает
+import java.util.Scanner;
+
+public class Polyndrom {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите слово:");
+        String poly = in.nextLine();
+        //String[] polyMass = poly.split("");// преобразуем слово в массив букв
+        StringBuilder revPoly = new StringBuilder(poly).reverse();
+        System.out.println("++++" + revPoly);
+        if (revPoly.equals(poly)) System.out.println("Выраежение: " + poly + " - полиндром");
+        else System.out.println("Выражение: " + poly + " - не полиндром");
+    }
+}
